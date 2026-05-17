bad4fe519dbeca411767e1b564e709df4bf3f687
Siroshun09 <siro.shun8@gmail.com>
2026-05-17 22:21:44 +0900

    fix: use vanilla logic for checking rain

--- src/main/java/net/okocraft/pweatherriptidecanceler/PWeatherRiptideCanceler.java
+++ src/main/java/net/okocraft/pweatherriptidecanceler/PWeatherRiptideCanceler.java
@@ -1,10 +1,13 @@
 package net.okocraft.pweatherriptidecanceler;
 
-import org.bukkit.Location;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.levelgen.Heightmap;
 import org.bukkit.Material;
 import org.bukkit.WeatherType;
-import org.bukkit.World;
-import org.bukkit.block.Block;
+import org.bukkit.craftbukkit.CraftWorld;
+import org.bukkit.craftbukkit.util.CraftLocation;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -31,74 +34,64 @@ public final class PWeatherRiptideCanceler extends JavaPlugin implements Listene
         if (hand != EquipmentSlot.HAND && hand != EquipmentSlot.OFF_HAND) {
             return;
         }
+
         ItemStack trident = player.getInventory().getItem(hand);
-        if (trident == null || trident.getType() != Material.TRIDENT) {
+        if (trident.getType() != Material.TRIDENT) {
             return;
         }
-
-        trident = trident.clone();
 
         int riptide = trident.getEnchantmentLevel(Enchantment.RIPTIDE);
         if (riptide == 0 || player.isInWater()) {
             return;
         }
 
-        if (isInRain(player, true) && !isInRain(player, false)) {
-            if (hand == EquipmentSlot.OFF_HAND) {
-                PlayerInventory inv = player.getInventory();
-                ItemStack mainHandItem = inv.getItemInMainHand();
-                inv.setItemInMainHand(inv.getItemInOffHand());
-                inv.setItemInOffHand(null);
-                player.dropItem(true);
-                inv.setItemInMainHand(mainHandItem);
-            } else {
-                player.dropItem(true);
-            }
-            player.updateInventory();
-        }
-    }
-
-    public boolean isInRain(Player client, boolean clientSide) {
-        Location pos = client.getLocation();
-
-        return isRaining(client, pos, clientSide) || isRaining(client, new Location(
-                client.getWorld(),
-                pos.getX(),
-                client.getBoundingBox().getMaxY(),
-                pos.getZ()
-        ), clientSide);
-    }
-
-    public boolean isRaining(Player client, Location pos, boolean clientSide) {
-        World world = pos.getWorld();
-        if (world == null) {
-            return false;
+        if (!(player.getWorld() instanceof CraftWorld craftWorld)) {
+            return;
         }
 
-        Block block = world.getBlockAt(pos);
+        Level level = craftWorld.getHandle();
+        BlockPos pos1 = CraftLocation.toBlockPos(player.getLocation());
+        BlockPos pos2 = new BlockPos(pos1.getX(), (int) Math.floor(player.getBoundingBox().getMaxY()), pos1.getZ());
 
-        if ((clientSide && client.getPlayerWeather() != WeatherType.DOWNFALL) || (!clientSide && !world.hasStorm())) {
-            return false;
-        } else if (block.getLightFromSky() < 15) {
-            return false;
-        } else if (world.getHighestBlockYAt(pos.getBlockX(), pos.getBlockZ()) > pos.getY()) {
-            return false;
+        if (!isRainingAtForClient(player, level, pos1, pos2) || isRainingAtForServer(level, pos1, pos2)) {
+            return;
+        }
+
+        if (hand == EquipmentSlot.OFF_HAND) {
+            PlayerInventory inv = player.getInventory();
+            ItemStack mainHandItem = inv.getItemInMainHand();
+            inv.setItemInMainHand(inv.getItemInOffHand());
+            inv.setItemInOffHand(null);
+            player.dropItem(true);
+            inv.setItemInMainHand(mainHandItem);
         } else {
-            return canRainAtPositionedBiome(pos) && block.getTemperature() >= 0.15F;
+            player.dropItem(true);
         }
+
+        player.updateInventory();
     }
 
-    public boolean canRainAtPositionedBiome(Location pos) {
-        World world = pos.getWorld();
-        if (world == null || world.getEnvironment() != World.Environment.NORMAL) {
-            return false;
+    public boolean isRainingAtForServer(Level level, BlockPos pos1, BlockPos pos2) {
+        return level.isRainingAt(pos1) || level.isRainingAt(pos2);
+    }
+
+    // See: net.minecraft.world.level.Level#isRainingAt
+    public boolean isRainingAtForClient(Player client, Level level, BlockPos pos1, BlockPos pos2) {
+        return precipitationAtForClient(client, level, pos1) == Biome.Precipitation.RAIN ||
+               precipitationAtForClient(client, level, pos2) == Biome.Precipitation.RAIN;
+    }
+
+    // See: net.minecraft.world.level.Level#precipitationAt
+    public Biome.Precipitation precipitationAtForClient(Player client, Level level, BlockPos pos) {
+        if (client.getPlayerWeather() != WeatherType.DOWNFALL) {
+            return Biome.Precipitation.NONE;
+        } else if (!level.canSeeSky(pos)) {
+            return Biome.Precipitation.NONE;
+        } else if (level.getHeightmapPos(Heightmap.Types.MOTION_BLOCKING, pos).getY() > pos.getY()) {
+            return Biome.Precipitation.NONE;
+        } else {
+            Biome biome = level.getBiome(pos).value();
+            return biome.getPrecipitationAt(pos, level.getSeaLevel());
         }
-        return switch (world.getBiome(pos)) {
-            case DESERT,
-                    SAVANNA, SAVANNA_PLATEAU, WINDSWEPT_SAVANNA,
-                    BADLANDS, ERODED_BADLANDS, WOODED_BADLANDS,
-                    THE_VOID -> false;
-            default -> true;
-        };
     }
 }
